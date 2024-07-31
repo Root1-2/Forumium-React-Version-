039e4998ab00082d9b55e95436d2232c0a8952b4
John Doe <abdullahalmasrur8@gmail.com>
2024-08-01 00:29:18 +0600

    Routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+
+import Homepage from "./pages/Homepage";
+import Profile from "./pages/Profile";
+import Setting from "./pages/Setting";
+import Post from "./pages/Post";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
   return (
-    <div className="m-5">
-      <p className="text-center text-3xl text-yellow-500">Forumium</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="homepage" />} />
+          <Route path="homepage" element={<Homepage />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="setting" element={<Setting />} />
+          <Route path="post/:id" element={<Post />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
