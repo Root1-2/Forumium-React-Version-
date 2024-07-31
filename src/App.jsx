8dfccab80ca4d924f7bdc43044e3538c7e6d73fb
John Doe <abdullahalmasrur8@gmail.com>
2024-08-01 00:01:46 +0600

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="m-5">
-      <p className="text-center text-3xl text-yellow-500">Hello Vite Boom</p>
+      <p className="text-center text-3xl text-yellow-500">Forumium</p>
     </div>
   );
 }
