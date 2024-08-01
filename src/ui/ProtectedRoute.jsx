c42164a41f8b139a825f78eb6b36e83db01ae3e7
John Doe <abdullahalmasrur8@gmail.com>
2024-08-01 23:56:55 +0600

    Spinnin component

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,4 +1,17 @@
+/* eslint-disable react/prop-types */
+import { useUser } from "../services/useUser";
+import Spinner from "./Spinner";
+
 function ProtectedRoute({ children }) {
+  const { user, isLoading } = useUser();
+
+  if (isLoading)
+    return (
+      <div className="flex h-screen items-center justify-center bg-gray-50">
+        <Spinner />
+      </div>
+    );
+
   return children;
 }
 
