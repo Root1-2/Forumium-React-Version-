f5f70d95695941568789852fbcea8f3ce90f8ade
John Doe <abdullahalmasrur8@gmail.com>
2024-08-04 02:11:02 +0600

    #

--- src/components/ForumTableRow.jsx
+++ src/components/ForumTableRow.jsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export default function ForumTableRow({ post }) {
-  const { postName, postCreator, created_at, postSection } = post;
+  const { postName, postCreator, created_at, postSection, id } = post;
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate(`/post/${id}`);
+  }
   return (
-    <tr className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
+    <tr
+      className="cursor-pointer border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
+      onClick={handleClick}
+    >
       <th
         scope="row"
         className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
