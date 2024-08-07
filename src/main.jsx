48d050b54df79515b5d9dee5b016af30b6136a5a
John Doe <abdullahalmasrur8@gmail.com>
2024-08-07 17:56:14 +0600

    #

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-window.addEventListener("load", () => {
-  window.location.reload();
-});
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
