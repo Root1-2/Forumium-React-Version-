1f1383561d5bc70be611a418f1808c4923661101
John Doe <abdullahalmasrur8@gmail.com>
2024-08-07 17:47:19 +0600

    #

--- src/services/useSignUp.js
+++ src/services/useSignUp.js
@@ -1,14 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 import { signUp as signUpApi } from "./apiAuth";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export function useSignUp() {
+  const navigate = useNavigate();
   const { mutate: signUp, isPending } = useMutation({
     mutationFn: signUpApi,
     onSuccess: (user) => {
       toast.success(
         "Account Create Successfully.! Please Verify The new account from user's email address",
       );
+      navigate("/login", { replace: true });
     },
     onError: (user) => {
       toast.error("Account Creation Failed");
