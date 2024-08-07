3da97352027202b07437242119f9274c381f815f
John Doe <abdullahalmasrur8@gmail.com>
2024-08-07 20:55:03 +0600

    #

--- src/services/useUser.js
+++ src/services/useUser.js
@@ -1,15 +1,10 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getCurrentUser } from "./apiAuth";
 
 export function useUser() {
-  const queryClient = useQueryClient();
   const { isPending, data: user } = useQuery({
     queryKey: ["user"],
     queryFn: getCurrentUser,
-    onSuccess: (user) => {
-      queryClient.setQueryData(["user"], user.user);
-    },
-    staleTime: Infinity,
   });
 
   return { isPending, user, isAuthenticated: user?.role === "authenticated" };
