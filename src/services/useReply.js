f92cc0826b27022be9c600964126a6a9e069b8d2
John Doe <abdullahalmasrur8@gmail.com>
2024-08-07 16:15:39 +0600

    dynamically refetched the replies

--- src/services/useReply.js
+++ src/services/useReply.js
@@ -1,11 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { getReply as getReplyApi } from "./apiReply";
+import { useEffect } from "react";
+import supabase from "./supabase";
 
 export function useReply(id) {
-  const { isPending, data: replies } = useQuery({
+  const {
+    isPending,
+    data: replies,
+    refetch,
+  } = useQuery({
     queryKey: ["reply", id],
     queryFn: () => getReplyApi(id),
+    refetchOnWindowFocus: false,
   });
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("replies")
+      .on(
+        "postgres_changes",
+        { event: "*", table: "replies", filter: `postId=eq.${id}` },
+        () => refetch(),
+      )
+      .subscribe();
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, [id, refetch]);
 
   return { isPending, replies };
 }
