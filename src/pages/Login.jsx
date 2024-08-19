04d346e765cd0b81a0b9f8422a14b5cc51d720c7
John Doe <abdullahalmasrur8@gmail.com>
2024-08-19 23:46:28 +0600

    #

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,8 @@ import Logo from "../ui/Logo";
 
 function Login() {
   return (
-    <div className="flex h-screen items-center">
-      <div className="mx-auto rounded-lg bg-gray-700 px-10 py-7">
+    <div className="flex h-screen items-center justify-center">
+      <div className="rounded-lg bg-gray-700 px-0 py-7 sm:px-10">
         <div className="mb-8 flex justify-center">
           <Logo />
         </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,8 +3,8 @@ import SignUpForm from "../components/SignUpForm";
 
 function SignUp() {
   return (
-    <div className="flex h-screen items-center">
-      <div className="mx-auto rounded-lg bg-gray-700 px-20 py-8">
+    <div className="flex h-screen items-center justify-center">
+      <div className="rounded-lg bg-gray-700 px-0 py-0 sm:px-20 sm:py-8">
         <div className="mb-8 flex justify-center">
           <Logo />
         </div>
